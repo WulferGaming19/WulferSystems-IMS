ab7da92be3016eca66ef5ef4ae81409fcf9369fc
Wulfer / Stefan <wulfergaming19@gmail.com>
2025-04-21 01:05:07 +0200

    Löschung von durch fehler entsandene Dateien

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-import { Client, GatewayIntentBits } from 'discord.js';
-import * as dotenv from 'dotenv';
-dotenv.config();
-const client = new Client({
-    intents: [GatewayIntentBits.Guilds],
-});
-console.log('🚀 Der legendäre Bot wird gestartet...');
