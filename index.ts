f4a99945c9d6cdc34e8dc78deec6376b914e4470
Wulfer / Stefan <wulfergaming19@gmail.com>
2025-04-21 12:32:58 +0200

    Anfang der Integrierung einer Datenbank mit einem Test der, der den letzten Start speichert. Umstrukturierung der Datenbank datei

--- index.ts
+++ index.ts
@@ -1,25 +1,12 @@
 import { Client, GatewayIntentBits, Events } from 'discord.js'; // Import der Module aus discord.js// für die .env Datei
 import dotenv from 'dotenv-safe';
-import Database from 'better-sqlite3';
+import { initBotStatus } from './src/db/database.js';
 import { performance } from 'node:perf_hooks';
 
 import { log } from './src/utils/logging.js';
 
 // laden der .env Datei
 dotenv.config();
-
-// Datenbank Pfad setzung
-const db = new Database('src/db/botdata.db');
-
-// Hier dann als Test eine Speicherung des letzten Bot startes in der Datenbank
-db.prepare(
-  `
-  CREATE TABLE IF NOT EXISTS bot_status (
-    id INTEGER PRIMARY KEY,
-    last_start TEXT
-  )
-`
-).run();
 
 const startTime = performance.now(); // ⏱ Start
 
@@ -32,18 +19,16 @@ const client = new Client({
 client.once(Events.ClientReady, async () => {
   log('success', `Erfolgreich eingeloggt als: ${client.user?.tag}`);
   log('success', `${client.user?.tag} ist erfolgreich gestartet!`);
+  initBotStatus(); // Startzeit in DB speichern
 
   const endTime = performance.now(); // ⏱ Stop
-  const duration = (endTime - startTime).toFixed(2);
-  log('info', `Startzeit: ${duration} ms`); // Info Log
+  const duration = (endTime - startTime).toFixed(2); // Annahme der Zeit
+  log('info', `Startzeit: ${duration} ms`); // Info Log mit der dauer des startens
 });
 
-// Fehlermeldung, wenn kein Token angegeben worden ist oder ein feher auftritt
-// # Recherce für Fehlermeldung erforderlich
-
-// Abfrage und auszug des Bot Tokens aus der .env
+// Fehlermeldung, wenn der Bott nicht starten kann
 client.login(process.env.DISCORD_TOKEN).catch((error) => {
   log('error', 'Bot konnte nicht starten!');
-  log('error', `Grund: ${error.message || error}`);
-  process.exit(1);
+  log('error', `Grund: ${error.message || error}`); // Grund für das nicht starten
+  process.exit(1); // Beendigung der Instanz
 });
--- src/db/database.ts
+++ src/db/database.ts
@@ -1,1 +1,25 @@
-// Datei dafür da, um die index.ts nicht vollzumüllen und den start des Botes zu verschnellern
+import Database from 'better-sqlite3';
+
+const db = new Database('./data/botdata.db');
+
+export function initBotStatus() {
+  db.prepare(
+    `
+      CREATE TABLE IF NOT EXISTS bot_status (
+        id INTEGER PRIMARY KEY,
+        last_start TEXT
+      )
+    `
+  ).run();
+
+  const now = new Date().toISOString();
+
+  db.prepare(
+    `
+      INSERT OR REPLACE INTO bot_status (id, last_start)
+      VALUES (1, ?)
+    `
+  ).run(now);
+}
+
+export default db;
