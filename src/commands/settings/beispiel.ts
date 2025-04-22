51b0cca86422dcc0bdcbb77ff6b4450227bc5ed8
Wulfer / Stefan <wulfergaming19@gmail.com>
2025-04-22 14:43:42 +0200

    Kleiner Slash command zum testen der Funktionalität

--- src/commands/settings/beispiel.ts
+++ src/commands/settings/beispiel.ts
@@ -7,7 +7,23 @@ import {
 // 📦 Exportiere den Command mit Name und Beschreibung
 export const data = new SlashCommandBuilder()
   .setName('beispiel')
-  .setDescription('Ein funktionierender Testcommand');
+  .setDescription('Ein funktionierender Testcommand')
+  .addSubcommand((subcommand) =>
+    subcommand
+      .setName('test')
+      .setDescription('Ein Test Subcommand')
+      .addStringOption((option) =>
+        option
+          .setName('text')
+          .setDescription('Der Text, der angezeigt werden soll')
+          .setRequired(true)
+      )
+  );
+
+// .addSubCommand((subcommand) =>           //   subcommand
+//     subcommand   //       .setName('test')               //   setName
+//       .setDescription('Ein Test Subcommand') //   setDescription     //       .addStringOption((option) =>           //   addStringOption    //         option           //       .setName('text')               //   setName
+//           .setDescription('Der Text, der angezeigt werden soll') //   setDescription     //         .setRequired(true)           //   setRequired
 
 // 🚀 Die eigentliche Funktion, die bei Ausführung aufgerufen wird
 export async function execute(interaction: ChatInputCommandInteraction) {
@@ -15,4 +31,7 @@ export async function execute(interaction: ChatInputCommandInteraction) {
     content: '✅ Dieser Command funktioniert!',
     flags: MessageFlags.Ephemeral,
   });
+}
+function addSubCommand(arg0: (subcommand: any) => any) {
+  throw new Error('Function not implemented.');
 }
